3ab27fc106392ebcf5cd56cc63058a95390932e9
jimmy_on_class <jimmyli88623@gmail.com>
2024-06-04 18:31:24 +0800

    implement function in food detail page

--- lib/Food Illustration/FoodDetail.dart
+++ lib/Food Illustration/FoodDetail.dart
@@ -9,7 +9,6 @@ class FoodDetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     String allReminders = item['cn_remind'].join('、');
 
     return Theme(
@@ -18,64 +17,110 @@ class FoodDetailPage extends StatelessWidget {
         appBar: AppBar(
             // title: Text(item['cn']),
             ),
-        body: Padding(
-          padding: EdgeInsets.all(15.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  ClipOval(
-                    child: Image.network(
-                      item['img_url'],
-                      fit: BoxFit.contain,
-                      width: 100,
-                      height: 100,
-                    ),
-                  ),
-                  SizedBox(width: MediaQuery.of(context).size.width * 0.1),
-                  Text(
-                    item['cn'],
-                    style: const TextStyle(
-                        fontSize: 40, fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-              SizedBox(height: MediaQuery.of(context).size.height * 0.1),
-              const Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    '介紹:',
-                    style: TextStyle(fontSize: 25),
-                  )),
-              SizedBox(height: MediaQuery.of(context).size.height * 0.05),
-              Text(
-                item['cn_introduce'],
-                style: const TextStyle(fontSize: 20),
-              ),
-              SizedBox(height: MediaQuery.of(context).size.height * 0.2),
-              if (item['cn_remind'] != null && item['cn_remind'].isNotEmpty)
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
                 Row(
                   children: [
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.dangerous),
-                    ),
-                    Expanded(
-                      child: Text(
-                       allReminders,
-                        style: const TextStyle(fontSize: 20),
+                    ClipOval(
+                      child: Image.network(
+                        item['img_url'],
+                        fit: BoxFit.contain,
+                        width: 100,
+                        height: 100,
                       ),
                     ),
+                    SizedBox(width: MediaQuery.of(context).size.width * 0.1),
+                    Text(
+                      item['cn'],
+                      style: const TextStyle(
+                          fontSize: 40, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(width: MediaQuery.of(context).size.width * 0.1),
+                    Column(
+                      children: [
+                        IconButton(onPressed: () {}, icon: Icon(Icons.star)),
+                        IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.record_voice_over)),
+                      ],
+                    )
                   ],
                 ),
-              // TextButton(
-              //   onPressed: () {
-              //     Navigator.of(context).pop();
-              //   },
-              //   child: const Text('Close'),
-              // ),
-            ],
+                SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+                const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      '介紹:',
+                      style: TextStyle(fontSize: 25),
+                    )),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+                Text(
+                  item['cn_introduce'],
+                  style: const TextStyle(fontSize: 20),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.2),
+                if (item['cn_remind'] != null && item['cn_remind'].isNotEmpty)
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: Icon(Icons.warning_amber),
+                                content: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Text(item['jn_remind'].join(',')),
+                                    const SizedBox(height: 10),
+                                    Text(item['cn_remind'].join(',')) ,
+                                    const SizedBox(height: 10),
+                                    Image.network(item['img_url']),//
+                                    const SizedBox(height: 10),
+                                    Text(item['cn_key_word'].join(',')),
+                                    const SizedBox(height: 10),
+                                    Text(item['jn_key_word'].join(','))
+                                    // 替换为你想要添加的内容
+                                  ],
+                                ),
+                                actions: [
+                                  Center(
+                                    child: IconButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      icon: const Text('Close'),
+                                    ),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        },
+                        icon: const Icon(Icons.warning_amber),
+                      ),
+                      Expanded(
+                        child: Text(
+                          allReminders,
+                          style: const TextStyle(fontSize: 20),
+                        ),
+                      ),
+                    ],
+                  ),
+                // TextButton(
+                //   onPressed: () {
+                //     Navigator.of(context).pop();
+                //   },
+                //   child: const Text('Close'),
+                // ),
+              ],
+            ),
           ),
         ),
       ),
